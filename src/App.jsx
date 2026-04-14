ae27d4194149e997e5b35f6449724c1c423ff495
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2026-04-15 00:05:19 +0530

    Initial commit: setup project structure and install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="bg-[#F6EDE9]">
+      <h1 className="text-2xl text-black">Alchelyst-Assignment</h1>
+    </div>
+  )
+}
+
+export default App
